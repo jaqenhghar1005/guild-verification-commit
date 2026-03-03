8d05675f8fe62a148cc07f98c28f2407712e25b5
bahadır kökçü <lokiasgard1005@gmail.com>
2026-03-03 17:34:18 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:34:16.892Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-03T14:34:18.351Z
+Commit: Add multi-language support
