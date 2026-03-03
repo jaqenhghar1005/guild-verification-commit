11e07f09c196ebbe818f204dd26bd0b2c95918a0
bahadır kökçü <lokiasgard1005@gmail.com>
2026-03-03 17:34:32 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:34:28.068Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-03T14:34:31.977Z
+Commit: Resolve null pointer exception in user service
