895ffece26324a76128fd9d336840562430dce59
bahadır kökçü <lokiasgard1005@gmail.com>
2026-03-03 17:34:01 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:33:39.509Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-03T14:34:00.877Z
+Commit: Fix memory leak in event handlers
