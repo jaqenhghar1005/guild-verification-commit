2ee7fcb37d2e7ca7c2896dcf4d21b8ef1be6edf0
bahadır kökçü <lokiasgard1005@gmail.com>
2026-03-03 17:34:28 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:34:19.841Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-03T14:34:28.068Z
+Commit: Introduce real-time notifications
