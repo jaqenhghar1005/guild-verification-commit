901cb9db1d8843ed1891dd12b9125c14b25c3ac8
bahadır kökçü <lokiasgard1005@gmail.com>
2026-03-12 01:50:44 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:33:54.679Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-11T22:50:43.640Z
+Commit: Fix broken links in documentation
